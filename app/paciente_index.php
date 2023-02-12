cb01f62f1d9f002c04a278149609d69f77589abb
Diego Neves <pdcassiano@gmail.com>
2023-02-12 13:02:54 -0300

    section intitucional

--- app/paciente_index.php
+++ app/paciente_index.php
@@ -12,7 +12,7 @@
     <nav class='paciente-menu-topo'>
         <ul>
             <li class="paciente-menu-topo-link"><a href="#">home</a></li>
-            <li class="paciente-menu-topo-link"><a href="#">quem somos</a></li>
+            <li class="paciente-menu-topo-link"><a href="#institucional">quem somos</a></li>
             <li class="paciente-menu-topo-link"><a href="#especialidades">especialidade</a></li>
             <li class="paciente-menu-topo-link"><a href="#">convênios</a></li>
             <li class="paciente-menu-topo-link"><a href="#">contatos</a></li>
@@ -30,7 +30,8 @@
         <img src="<?=BASEURL?>assets/images/banner/meia_elipse.png" id='meia_elipse' alt="">
         <h2>Seu<br>mundo<br>com um<br>novo<br>olhar</h2>
     </div>
-    <div class="cards" id='especialidades'>
+    <div class="cards">
+        <span class="identificador" id='especialidades'></span>
         <article class="card" id='cirurgia-refrativa'>
             <div class="card-front">
                 <img src="<?=BASEURL?>assets/images/cards/avatar-refrativa.png" alt="" class="avatar">
@@ -68,6 +69,55 @@
                 <p>Altíssimo nível de segurança e precisão, com excelentes resultados.</p>
             </div>
         </div>
+    </section>
+    <section class="dobra institucional">
+    <span class="identificador" id='institucional'></span>
+    <div class="grid">
+        <div class="institucional-item quem-somos">
+            <h2 class="institucional-item-title">Quem Somos</h2>
+            <div class="institucional-item-text">
+                <p>Localizada em Maceió, Alagoas, desde 2009, somos referência no Brasil e no mundo em cirurgias refrativas a laser.</p>
+                <p>Oferecemos o que há de melhor em tecnologia e inovação para garantir a segurança e precisão em nossos procedimentos.</p>
+                <p>Contamos com excelentes profissionais e uma equipe médica da mais alta expertise, visando também no atendimento humanizado.</p>
+            </div>
+        </div>
+        <div class="institucional-item missao">
+            <h2 class="institucional-item-title">Missão</h2>
+            <div class="institucional-item-text">
+                <p>Oferecer o que há de melhor em cirurgia refrativa (miopia, hipermetropia e astigmatismo), ceratocone e tratamento da síndrome do olho seco com segurança humanização, tecnologia de ponta e inovação.</p>
+            </div>
+        </div>
+        <div class="institucional-item visao">
+            <h2 class="institucional-item-title">Visão</h2>
+            <div class="institucional-item-text">
+                <p>Ser referência em tratamento ocular a laser com tecnologia de última geração.
+                </p>
+            </div>
+        </div>
+        <div class="institucional-item proposito">
+            <h2 class="institucional-item-title">Propósito</h2>
+            <div class="institucional-item-text">
+                <p>Proporcionar segurança e atendimento de excelência a pacientes e oftalmologistas através de um ambiente confortável, humanizado e com o que há de mais moderno em tecnologia.</p>
+            </div>
+        </div>
+        <div class="institucional-item valores">
+            <h2 class="institucional-item-title">Valores</h2>
+            <ul class="institucional-item-text">
+              <li>Desenvolvimento profissional;</li>
+              <li>Valorização do ser humanto (equipe e pacientes);</li>
+              <li>Ética nas relações;</li>
+              <li>Parcerias apoiadas em princípios.</li>
+            </ul>
+        </div>
+    </div>
+  </section>
+    <section class="dobra">
+    <span class="identificador" id='depoimentos'></span>
+
+    </section>
+    <section class="dobra">
+    <span class="identificador" id='faq'></span>
+
     </section>
 
 </main>
